b35d98d5fe395e941f7040818534165e832cba0b
Prateek Varma <prtk.varma@gmail.com>
2022-11-28 22:15:28 -0600

    Placed enum in it's own file, and used it as 'Type Assertion' in returning a mapped array

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResults';
 
 export class CsvFileReader {
     data: string[][] = [];
@@ -16,12 +17,16 @@ export class CsvFileReader {
             return row.split(',')
         })
         .map((row: string[]): any => {
+            //below this converts all elements of this array into some or the other type.
+            //each row is a row from the CSV file
             return [
                 dateStringToDate(row[0]), // Date
                 row[1], //team
                 row[2], //team
                 parseInt(row[3]), //matches
                 parseInt(row[4]), //matches
+                row[5] as MatchResult, //string of match result. We're overriding TS's default behavior (called 'Type Assertion') - telling it that this entry is of type MatchResult enum (H, A or D).
+                row[6] //leaving as it is
             ];
         });
     }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResults";
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
-
-enum MatchResult {
-    HomeWin = 'H',
-    AwayWin = 'A',
-    Draw = 'D'
-};
 
 let manUnitedWins = 0; //checks how many games Man united won
 

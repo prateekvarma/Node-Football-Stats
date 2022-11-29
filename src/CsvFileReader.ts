01229c6b9acd6cc0b8dd0d40f10d2722b089ee49
Prateek Varma <prtk.varma@gmail.com>
2022-11-28 22:27:14 -0600

    defined a Tuple to type annotate each row from the csv file

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,8 +2,11 @@ import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResults';
 
+// Below, we're defining a tuple - accoding to each row for the CSV file
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-    data: string[][] = [];
+    data: MatchData[] = [];
 
     constructor(public filename: string) {} //defining the public modifier will auto create a property called filename, and instantiate whenever the argument is passed when creating an instance of this class
 
@@ -16,7 +19,7 @@ export class CsvFileReader {
         .map((row: string): string[] => {
             return row.split(',')
         })
-        .map((row: string[]): any => {
+        .map((row: string[]): MatchData => {
             //below this converts all elements of this array into some or the other type.
             //each row is a row from the CSV file
             return [
@@ -26,7 +29,7 @@ export class CsvFileReader {
                 parseInt(row[3]), //matches
                 parseInt(row[4]), //matches
                 row[5] as MatchResult, //string of match result. We're overriding TS's default behavior (called 'Type Assertion') - telling it that this entry is of type MatchResult enum (H, A or D).
-                row[6] //leaving as it is
+                row[6] //leaving as it is, not converting data type
             ];
         });
     }

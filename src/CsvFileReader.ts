01fc4bfd0021c407835ea0052a85244bc231993a
Prateek Varma <prtk.varma@gmail.com>
2022-11-29 14:47:44 -0600

    Refactored mapping each row and type-casting functionality into it's separate function

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -19,18 +19,20 @@ export class CsvFileReader {
         .map((row: string): string[] => {
             return row.split(',')
         })
-        .map((row: string[]): MatchData => {
-            //below this converts all elements of this array into some or the other type.
-            //each row is a row from the CSV file
-            return [
-                dateStringToDate(row[0]), // Date
-                row[1], //team
-                row[2], //team
-                parseInt(row[3]), //matches
-                parseInt(row[4]), //matches
-                row[5] as MatchResult, //string of match result. We're overriding TS's default behavior (called 'Type Assertion') - telling it that this entry is of type MatchResult enum (H, A or D).
-                row[6] //leaving as it is, not converting data type
-            ];
-        });
+        .map(this.mapRow);
+    }
+
+    mapRow(row: string[]): MatchData {
+        //below this converts all elements of this array into some or the other type.
+        //each row is a row from the CSV file
+        return [
+            dateStringToDate(row[0]), // Date
+            row[1], //team
+            row[2], //team
+            parseInt(row[3]), //matches
+            parseInt(row[4]), //matches
+            row[5] as MatchResult, //string of match result. We're overriding TS's default behavior (called 'Type Assertion') - telling it that this entry is of type MatchResult enum (H, A or D).
+            row[6] //leaving as it is, not converting data type
+        ];
     }
 }
